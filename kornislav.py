e03c9eec34ed9c8186c66064016d656ee64b0d27
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2019-02-27 21:05:53 +0000

    Removing CORS

--- kornislav.py
+++ kornislav.py
@@ -8,27 +8,9 @@ from json import dumps
 app = Bottle()
 
 
-"""Decorator used to enable CORS on HTTP Responses"""
-"""Code taken from https://stackoverflow.com/questions/17262170/bottle-py-enabling-cors-for-jquery-ajax-requests"""
-def enable_cors(fn):
-    def _enable_cors(*args, **kwargs):
-        # set CORS headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-
-        if bottle.request.method != 'OPTIONS':
-            # actual request; reply with the actual response
-            return fn(*args, **kwargs)
-
-    return _enable_cors
-
-
-
 
 """Root Response"""
 @app.route('/')
-@enable_cors
 def hello():
     return "Hello, World!"
 
@@ -36,7 +18,6 @@ def hello():
 
 """Signals Game Start"""
 @app.post('/start')
-@enable_cors
 def start():
 
     try:
@@ -61,7 +42,6 @@ def ping():
 
 """Server is requesting a Move"""
 @app.post('/move')
-@enable_cors
 def move():
     try:
         data = request.json
