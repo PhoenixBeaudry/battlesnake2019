712f1f31729759c2aa3b046e8263140fad022971
greylogo <36058382+greylogo@users.noreply.github.com>
2019-02-28 22:50:45 -0800

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -6,7 +6,7 @@ def poly_decay(weight, poly):
 	if weight>0:
 		return lambda depth: (weight - depth**poly) if (weight - depth**poly) > 0 else 0
 	else:
-		return lambda depth: (weight + depth**poly) if (weight - depth**poly) < 0 else 0
+		return lambda depth: (weight + depth**poly) if (weight + depth**poly) < 0 else 0
 
 def self_function(depth):
 	return 1000000
