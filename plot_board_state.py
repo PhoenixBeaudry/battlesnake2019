60d1622fd451472e2736f1dc1b7fcf25dd16c60c
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2019-03-02 18:54:31 +0000

    new edge tester

--- plot_board_state.py
+++ plot_board_state.py
@@ -71,7 +71,7 @@ defaultdata = {
   }
 }
 
-testdata = {'game': {'id': 'f1d4600b-91ce-424b-b7b1-64505442de57'}, 'turn': 458, 'board': {'height': 7, 'width': 7, 'food': [{'x': 5, 'y': 2}, {'x': 5, 'y': 3}, {'x': 4, 'y': 3}, {'x': 3, 'y': 1}, {'x': 4, 'y': 1}, {'x': 3, 'y': 2}, {'x': 5, 'y': 1}, {'x': 4, 'y': 2}, {'x': 3, 'y': 3}], 'snakes': [{'id': 'gs_8fFRqghPjmQ3dd38TWPTCPhB', 'name': 'PhoenixBeaudry / NodeSlave', 'health': 95, 'body': [{'x': 0, 'y': 6}, {'x': 0, 'y': 5}, {'x': 0, 'y': 4}, {'x': 0, 'y': 3}, {'x': 0, 'y': 2}, {'x': 0, 'y': 1}, {'x': 0, 'y': 0}, {'x': 1, 'y': 0}, {'x': 2, 'y': 0}, {'x': 3, 'y': 0}, {'x': 4, 'y': 0}, {'x': 5, 'y': 0}, {'x': 6, 'y': 0}, {'x': 6, 'y': 1}, {'x': 6, 'y': 2}, {'x': 6, 'y': 3}, {'x': 6, 'y': 4}, {'x': 5, 'y': 4}, {'x': 4, 'y': 4}, {'x': 3, 'y': 4}, {'x': 2, 'y': 4}, {'x': 2, 'y': 3}, {'x': 2, 'y': 2}, {'x': 2, 'y': 1}, {'x': 1, 'y': 1}, {'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 4}, {'x': 1, 'y': 5}, {'x': 2, 'y': 5}, {'x': 3, 'y': 5}, {'x': 4, 'y': 5}, {'x': 5, 'y': 5}, {'x': 6, 'y': 5}, {'x': 6, 'y': 6}, {'x': 5, 'y': 6}, {'x': 4, 'y': 6}, {'x': 3, 'y': 6}, {'x': 2, 'y': 6}, {'x': 1, 'y': 6}]}]}, 'you': {'id': 'gs_8fFRqghPjmQ3dd38TWPTCPhB', 'name': 'PhoenixBeaudry / NodeSlave', 'health': 95, 'body': [{'x': 0, 'y': 6}, {'x': 0, 'y': 5}, {'x': 0, 'y': 4}, {'x': 0, 'y': 3}, {'x': 0, 'y': 2}, {'x': 0, 'y': 1}, {'x': 0, 'y': 0}, {'x': 1, 'y': 0}, {'x': 2, 'y': 0}, {'x': 3, 'y': 0}, {'x': 4, 'y': 0}, {'x': 5, 'y': 0}, {'x': 6, 'y': 0}, {'x': 6, 'y': 1}, {'x': 6, 'y': 2}, {'x': 6, 'y': 3}, {'x': 6, 'y': 4}, {'x': 5, 'y': 4}, {'x': 4, 'y': 4}, {'x': 3, 'y': 4}, {'x': 2, 'y': 4}, {'x': 2, 'y': 3}, {'x': 2, 'y': 2}, {'x': 2, 'y': 1}, {'x': 1, 'y': 1}, {'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 4}, {'x': 1, 'y': 5}, {'x': 2, 'y': 5}, {'x': 3, 'y': 5}, {'x': 4, 'y': 5}, {'x': 5, 'y': 5}, {'x': 6, 'y': 5}, {'x': 6, 'y': 6}, {'x': 5, 'y': 6}, {'x': 4, 'y': 6}, {'x': 3, 'y': 6}, {'x': 2, 'y': 6}, {'x': 1, 'y': 6}]}}
+#testdata = {'game': {'id': 'f1d4600b-91ce-424b-b7b1-64505442de57'}, 'turn': 458, 'board': {'height': 7, 'width': 7, 'food': [{'x': 5, 'y': 2}, {'x': 5, 'y': 3}, {'x': 4, 'y': 3}, {'x': 3, 'y': 1}, {'x': 4, 'y': 1}, {'x': 3, 'y': 2}, {'x': 5, 'y': 1}, {'x': 4, 'y': 2}, {'x': 3, 'y': 3}], 'snakes': [{'id': 'gs_8fFRqghPjmQ3dd38TWPTCPhB', 'name': 'PhoenixBeaudry / NodeSlave', 'health': 95, 'body': [{'x': 0, 'y': 6}, {'x': 0, 'y': 5}, {'x': 0, 'y': 4}, {'x': 0, 'y': 3}, {'x': 0, 'y': 2}, {'x': 0, 'y': 1}, {'x': 0, 'y': 0}, {'x': 1, 'y': 0}, {'x': 2, 'y': 0}, {'x': 3, 'y': 0}, {'x': 4, 'y': 0}, {'x': 5, 'y': 0}, {'x': 6, 'y': 0}, {'x': 6, 'y': 1}, {'x': 6, 'y': 2}, {'x': 6, 'y': 3}, {'x': 6, 'y': 4}, {'x': 5, 'y': 4}, {'x': 4, 'y': 4}, {'x': 3, 'y': 4}, {'x': 2, 'y': 4}, {'x': 2, 'y': 3}, {'x': 2, 'y': 2}, {'x': 2, 'y': 1}, {'x': 1, 'y': 1}, {'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 4}, {'x': 1, 'y': 5}, {'x': 2, 'y': 5}, {'x': 3, 'y': 5}, {'x': 4, 'y': 5}, {'x': 5, 'y': 5}, {'x': 6, 'y': 5}, {'x': 6, 'y': 6}, {'x': 5, 'y': 6}, {'x': 4, 'y': 6}, {'x': 3, 'y': 6}, {'x': 2, 'y': 6}, {'x': 1, 'y': 6}]}]}, 'you': {'id': 'gs_8fFRqghPjmQ3dd38TWPTCPhB', 'name': 'PhoenixBeaudry / NodeSlave', 'health': 95, 'body': [{'x': 0, 'y': 6}, {'x': 0, 'y': 5}, {'x': 0, 'y': 4}, {'x': 0, 'y': 3}, {'x': 0, 'y': 2}, {'x': 0, 'y': 1}, {'x': 0, 'y': 0}, {'x': 1, 'y': 0}, {'x': 2, 'y': 0}, {'x': 3, 'y': 0}, {'x': 4, 'y': 0}, {'x': 5, 'y': 0}, {'x': 6, 'y': 0}, {'x': 6, 'y': 1}, {'x': 6, 'y': 2}, {'x': 6, 'y': 3}, {'x': 6, 'y': 4}, {'x': 5, 'y': 4}, {'x': 4, 'y': 4}, {'x': 3, 'y': 4}, {'x': 2, 'y': 4}, {'x': 2, 'y': 3}, {'x': 2, 'y': 2}, {'x': 2, 'y': 1}, {'x': 1, 'y': 1}, {'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 4}, {'x': 1, 'y': 5}, {'x': 2, 'y': 5}, {'x': 3, 'y': 5}, {'x': 4, 'y': 5}, {'x': 5, 'y': 5}, {'x': 6, 'y': 5}, {'x': 6, 'y': 6}, {'x': 5, 'y': 6}, {'x': 4, 'y': 6}, {'x': 3, 'y': 6}, {'x': 2, 'y': 6}, {'x': 1, 'y': 6}]}}
 
 
 
@@ -102,10 +102,10 @@ if(plot):
   nx.draw_networkx(graph, pos, font_size=7)
 
   #Color food nodes
-  nx.draw_networkx_nodes(graph, pos, nodelist=gameboard.food, node_color='green')
+  #nx.draw_networkx_nodes(graph, pos, nodelist=gameboard.food, node_color='green')
 
   #Color self nodes
-  nx.draw_networkx_nodes(graph, pos, nodelist=gameboard.myself, node_color='blue')
+  #nx.draw_networkx_nodes(graph, pos, nodelist=gameboard.myself, node_color='blue')
 
   #Color enemy nodes
   enemylist = []
@@ -113,14 +113,20 @@ if(plot):
   	for enemypart in enemy:
   		enemylist.append(enemypart)
 
-  nx.draw_networkx_nodes(graph, pos, nodelist=enemylist, node_color='black')
+  #nx.draw_networkx_nodes(graph, pos, nodelist=enemylist, node_color='black')
 
   edges = {}
 
   for edge in graph.edges:
   	edges[edge] = graph[edge[0]][edge[1]]['weight']
 
-  nx.draw_networkx_edge_labels(graph, pos, edges, font_size=6, font_color='blue')
+  newedges = edges_of_depth_distance_two(graph, (5, 5), 6)
+
+  print(newedges)
+  
+  nx.draw_networkx_edges(graph, pos, edgelist=newedges, edge_color='red', width=6)
+
+  #nx.draw_networkx_edge_labels(graph, pos, edges, font_size=6, font_color='blue')
 
   plt.axis('off')
   plt.gca().invert_yaxis()
